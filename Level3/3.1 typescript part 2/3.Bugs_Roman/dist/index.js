73d531d47d781641c519582f3df78edda5461155
AntWader <AntWader@users.noreply.github.com>
2022-09-13 03:13:36 +0300

    recommit

--- Level3/3.1 typescript part 2/3.Bugs_Roman/dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var t;
-(function (t) {
-    t[t["one"] = 0] = "one";
-    t[t["two"] = 1] = "two";
-    t[t["three"] = 2] = "three";
-})(t || (t = {}));
-console.log(t.one);
--- Level3/3.1 typescript part 2/3.Bugs_Roman/src/index.ts
+++
@@ -1,7 +0,0 @@
-enum t {
-    "one",
-    "two",
-    "three"
-}
-
-console.log(t.one)
