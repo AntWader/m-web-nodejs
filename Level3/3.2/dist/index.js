83b71c828d0e8b332a1463dba8dd0288e100a077
AntWader <AntWader@users.noreply.github.com>
2022-09-26 22:52:49 +0300

    26.09.2022

--- Level3/3.2/dist/index.js
+++ Level3/3.2/dist/index.js
@@ -48,25 +48,101 @@ const port = 3000;
 let jsonParser = body_parser_1.default.json();
 // static frontend
 app.use('/', express_1.default.static(path.join(__dirname, '../frontend/')));
-app.get('*', jsonParser, function (req, res) {
+app.use(body_parser_1.default.urlencoded({ extended: true }));
+app.get('/', jsonParser, function (req, res) {
     return __awaiter(this, void 0, void 0, function* () {
         try {
+            console.log('/');
+            console.log(req.url);
             let books = JSON.parse(fs.readFileSync(path.join(__dirname, '../frontend/books-data.txt'), 'utf-8'));
-            if (req.url === '\/') { // url: /
+            if (/^\/$/.test(req.url)) {
                 let content = books.slice(0, 2);
                 res.send((0, booksTemplate_1.makeBooksPage)(content, books.length));
             }
-            if (/^\/\?/.test(req.url)) { // url starts from: /?
+            if (/^\/\?/.test(req.url)) {
+                // I NEED TO ADD SEARCH COMPONENTS !!!!!!!!!!
                 let body = querystring_1.default.parse(req.url.replace(/^\/\?/, ''));
-                console.log(req.url);
-                let content = books.length > body.offset ? books.slice(0, body.offset) : books;
-                res.send((0, booksTemplate_1.makeBooksPage)(content, books.length));
+                if (body.offset) {
+                    let content = books.length > body.offset ? books.slice(0, body.offset) : books;
+                    res.send((0, booksTemplate_1.makeBooksPage)(content, books.length));
+                }
+                else
+                    res.status(404).send({ error: "...." });
             }
-            if (/^\/book\//.test(req.url)) { // url starts from: /book/
-                let bookId = parseInt(req.url.replace(/^\/book\//, ''));
-                let book = books.find(el => el.id == bookId);
-                book ? res.send((0, bookTemplate_1.makeBookPage)(book)) : null;
-            }
+        }
+        catch (e) {
+            console.log(e);
+            res.status(500).send({ error: "...." }); //ERROR 500 server error
+        }
+    });
+});
+app.get('/book/:bookId', jsonParser, function (req, res) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            console.log('/book/:bookId');
+            console.log(req.url);
+            let books = JSON.parse(fs.readFileSync(path.join(__dirname, '../frontend/books-data.txt'), 'utf-8'));
+            let book = books.find(el => el.id == req.params.bookId);
+            book ? res.send((0, bookTemplate_1.makeBookPage)(book)) : res.status(404).send({ error: "...." });
+        }
+        catch (e) {
+            console.log(e);
+            res.status(500).send({ error: "...." }); //ERROR 500 server error
+        }
+    });
+});
+function authentication(req, res, next) {
+    let headerAuth = req.headers.authorization;
+    //console.log(req.headers);
+    if (!headerAuth) {
+        res.setHeader('WWW-Authenticate', 'Basic');
+        res.status(401).send({ error: "..." });
+    }
+    else {
+        // @ts-ignore
+        var auth = new Buffer.from(headerAuth.split(' ')[1], 'base64').toString().split(':');
+        var user = auth[0];
+        var pass = auth[1];
+        if (user === 'admin' && pass === 'admin') {
+            next();
+        }
+        else {
+            res.setHeader('WWW-Authenticate', 'Basic');
+            res.status(401).send({ error: "..." });
+        }
+    }
+}
+app.get('/admin', jsonParser, authentication, function (req, res) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            res.sendFile(path.join(__dirname, '../frontend/admin-page/admin-page.html'));
+        }
+        catch (e) {
+            console.log(e);
+            res.status(500).send({ error: "...." }); //ERROR 500 server error
+        }
+    });
+});
+app.get('/admin/api/v1/books', jsonParser, function (req, res) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            let books = JSON.parse(fs.readFileSync(path.join(__dirname, '../frontend/books-data.txt'), 'utf-8'));
+            res.send(JSON.stringify(books));
+        }
+        catch (e) {
+            console.log(e);
+            res.status(500).send({ error: "...." }); //ERROR 500 server error
+        }
+    });
+});
+app.post('/admin/api/v1/book', jsonParser, function (req, res) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            console.log(req.url);
+            console.log(req.body);
+            res.writeHead(302, {
+                Location: `http://localhost:${port}/admin/`
+            }).end();
         }
         catch (e) {
             console.log(e);
--- Level3/3.2/src/index.ts
+++ Level3/3.2/src/index.ts
@@ -19,29 +19,105 @@ let jsonParser = bodyParser.json();
 // static frontend
 app.use('/', express.static(path.join(__dirname, '../frontend/')));
 
-app.get('*', jsonParser, async function (req, res) {
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', jsonParser, async function (req, res) {
+    try {
+        console.log('/')
+        console.log(req.url)
+
+        let books = JSON.parse(fs.readFileSync(
+            path.join(__dirname, '../frontend/books-data.txt'), 'utf-8')) as bookType[]
+
+        if (/^\/$/.test(req.url)) {
+            let content = books.slice(0, 2)
+            res.send(makeBooksPage(content, books.length));
+        }
+        if (/^\/\?/.test(req.url)) {
+            // I NEED TO ADD SEARCH COMPONENTS !!!!!!!!!!
+            let body = querystring.parse(req.url.replace(/^\/\?/, '')) as unknown as { offset: number }
+
+            if (body.offset) {
+                let content = books.length > body.offset ? books.slice(0, body.offset) : books
+                res.send(makeBooksPage(content, books.length));
+            } else res.status(404).send({ error: "...." });
+        }
+    } catch (e) {
+        console.log(e);
+        res.status(500).send({ error: "...." }); //ERROR 500 server error
+    }
+})
+
+app.get('/book/:bookId', jsonParser, async function (req, res) {
+    try {
+        console.log('/book/:bookId')
+        console.log(req.url)
+
+        let books = JSON.parse(fs.readFileSync(
+            path.join(__dirname, '../frontend/books-data.txt'), 'utf-8')) as bookType[]
+
+        let book = books.find(el => el.id == req.params.bookId)
+
+        book ? res.send(makeBookPage(book)) : res.status(404).send({ error: "...." });
+    } catch (e) {
+        console.log(e);
+        res.status(500).send({ error: "...." }); //ERROR 500 server error
+    }
+});
+
+function authentication(req: express.Request, res: express.Response, next: () => void) {
+    let headerAuth = req.headers.authorization;
+    //console.log(req.headers);
+
+    if (!headerAuth) {
+        res.setHeader('WWW-Authenticate', 'Basic');
+        res.status(401).send({ error: "..." });
+    } else {
+        // @ts-ignore
+        var auth = new Buffer.from(headerAuth.split(' ')[1],
+            'base64').toString().split(':');
+        var user = auth[0];
+        var pass = auth[1];
+
+        if (user === 'admin' && pass === 'admin') {
+            next()
+        } else {
+            res.setHeader('WWW-Authenticate', 'Basic');
+            res.status(401).send({ error: "..." });
+        }
+    }
+}
+
+app.get('/admin', jsonParser, authentication, async function (req, res) {
+    try {
+        res.sendFile(path.join(__dirname, '../frontend/admin-page/admin-page.html'))
+    } catch (e) {
+        console.log(e);
+        res.status(500).send({ error: "...." }); //ERROR 500 server error
+    }
+});
+
+app.get('/admin/api/v1/books', jsonParser, async function (req, res) {
     try {
         let books = JSON.parse(fs.readFileSync(
             path.join(__dirname, '../frontend/books-data.txt'), 'utf-8')) as bookType[]
 
-        if (req.url === '\/') { // url: /
-            let content = books.slice(0, 2)
-            res.send(makeBooksPage(content, books.length));
-        }
-        if (/^\/\?/.test(req.url)) { // url starts from: /?
-            let body = querystring.parse(req.url.replace(/^\/\?/, '')) as unknown as { offset: number }
-            console.log(req.url)
+        res.send(JSON.stringify(books))
 
-            let content = books.length > body.offset ? books.slice(0, body.offset) : books
-            res.send(makeBooksPage(content, books.length));
-        }
-        if (/^\/book\//.test(req.url)) { // url starts from: /book/
-            let bookId = parseInt(req.url.replace(/^\/book\//, ''))
-            let book = books.find(el => el.id == bookId)
+    } catch (e) {
+        console.log(e);
+        res.status(500).send({ error: "...." }); //ERROR 500 server error
+    }
+});
 
-            book ? res.send(makeBookPage(book)) : null
-        }
+app.post('/admin/api/v1/book', jsonParser, async function (req, res) {
+    try {
+        console.log(req.url)
+        console.log(req.body)
 
+        res.writeHead(302, {
+            Location: `http://localhost:${port}/admin/`
+        }).end();
 
     } catch (e) {
         console.log(e);
