d6920d36b36d0cc092ded9f32f002f9611fb12bc
AntWader <AntWader@users.noreply.github.com>
2022-11-25 19:19:23 +0200

    25.11.2022

--- Level4/4.2/src/swapi/people/people.module.ts
+++ Level4/4.2/src/swapi/people/people.module.ts
@@ -4,9 +4,10 @@ import { PeopleController } from "./people.controller";
 import { DatabaseModule } from "src/database/database.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Person } from "../entities/person.entity";
+import { Gender } from "../entities/gender.entity";
 
 @Module({
-  imports: [DatabaseModule, TypeOrmModule.forFeature([Person])],
+  imports: [DatabaseModule, TypeOrmModule.forFeature([Person, Gender])],
   controllers: [PeopleController],
   providers: [PeopleService]
 })
