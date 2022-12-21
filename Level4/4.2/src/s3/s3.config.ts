54c77dd73263cbb9e981911393bd417573675331
AntWader <56272431+AntWader@users.noreply.github.com>
2022-12-21 08:11:03 +0200

    key update

--- Level4/4.2/src/s3/s3.config.ts
+++ Level4/4.2/src/s3/s3.config.ts
@@ -2,10 +2,6 @@ import { S3 } from "aws-sdk";
 
 export const s3ControllerPath = 's3/';
 
-const S3REGION = "eu-central-1";
-const S3KEY = "AKIASAT5PM3P5SO3EWG7";
-const S3SECRETKEY = "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";
-
 export const S3Config: S3.Types.ClientConfiguration = {
     region: S3REGION,
     accessKeyId: S3KEY,
