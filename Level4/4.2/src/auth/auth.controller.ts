f881c4394936b8625bd7eccc9cfff6dae6aad9a5
AntWader <56272431+AntWader@users.noreply.github.com>
2022-12-27 00:01:01 +0200

    26.12.2022

--- Level4/4.2/src/auth/auth.controller.ts
+++ Level4/4.2/src/auth/auth.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Post, Request, UseGuards } from "@nestjs/common";
 import { ApiBody } from "@nestjs/swagger";
 import { Request as RequestType } from 'express';
-import { UserLoginDto } from "src/auth_users/users.login.dto";
+import { UserLoginDto } from "src/auth/auth_users/users.login.dto";
 import { LocalAuthGuard } from "./local/local.auth.guard";
 import * as util from 'util';
 
