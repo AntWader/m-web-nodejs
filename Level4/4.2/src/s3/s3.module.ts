d087be2dd6bff5dd89616593a9020cb52bc77bf4
AntWader <56272431+AntWader@users.noreply.github.com>
2022-12-28 12:33:31 +0200

    comments correcting

--- Level4/4.2/src/s3/s3.module.ts
+++ Level4/4.2/src/s3/s3.module.ts
@@ -5,9 +5,6 @@ import { S3Service } from "./s3.service";
 /**
  * This module provides partial access to AWS S3 bucket, defined by S3Service functional.
  * Also uploaded files can be accessed through private connection from S3Controller.
- * 
- * You need to place S3Keys.ts file (with constants: S3REGION, S3KEY, S3SECRETKEY, BUCKET_NAME, USER_NAME) 
- * in the root of project to access you bucket!!!
  */
 @Module({
     controllers: [S3Controller],
